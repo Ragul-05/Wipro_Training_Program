8111de4a9de38a5c91d30e0337015621d9eca973
Ragul-05 <ragulgopinath2005@gmail.com>
2025-07-21 19:00:35 +0530

    Adding the file for KeyValueDigits.java

---
+++ MileStone_1/KeyValueDigits1.java
@@ -0,0 +1,19 @@
+package MileStone_1;
+public class KeyValueDigits1 {
+    public static void main(String[] args) {
+        int input1 = 5243, input2 = 2376, input3 = 2912;
+
+        int t1 = input1 / 1000, t2 = input2 / 1000, t3 = input3 / 1000;
+        int h1 = (input1 / 100) % 10, h2 = (input2 / 100) % 10, h3 = (input3 / 100) % 10;
+        int te1 = (input1 / 10) % 10, te2 = (input2 / 10) % 10, te3 = (input3 / 10) % 10;
+        int u1 = input1 % 10, u2 = input2 % 10, u3 = input3 % 10;
+
+        int thousands = Math.max(t1, Math.max(t2, t3));
+        int hundreds = Math.max(h1, Math.max(h2, h3));
+        int tens = Math.max(te1, Math.max(te2, te3));
+        int units = Math.max(u1, Math.max(u2, u3));
+
+        int key = thousands * 1000 + hundreds * 100 + tens * 10 + units;
+        System.out.println("Key Value: " + key);  
+    }
+}
