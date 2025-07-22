e63d7859be33899f7a14386a18358dfc657c0e33
Ragul-05 <ragulgopinath2005@gmail.com>
2025-07-22 12:39:21 +0530

    Adding the file for finding the Ones in inp1 , Tens in inp2and Maximum numer in inp3 to mapped it together for the result

---
+++ MileStone_1/KeyValue2.java
@@ -0,0 +1,32 @@
+package MileStone_1;
+
+public class KeyValue2 {
+    public static int getHundredsDigit(int num) {
+        return (num / 100) % 10;
+    }
+
+    public static int getTensDigit(int num) {
+        return (num / 10) % 10;
+    }
+
+    public static int getLargestDigit(int num) {
+        int max = 0;
+        while (num > 0) {
+            max = Math.max(max, num % 10);
+            num /= 10;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int input1 = 3252, input2 = 2451, input3 = 1354;
+
+        int hundreds = getHundredsDigit(input1);
+        int tens = getTensDigit(input2);
+        int maxDigit = getLargestDigit(input3);
+
+        int key = (hundreds * tens) -  maxDigit;
+        System.out.println("The final answers Key = " + key);
+    }
+
+}
